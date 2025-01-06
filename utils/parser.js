61849de6ce1e19d1d91c68178753ac2bbe450327
Raisa Primerova <riyuhide@gmail.com>
2025-01-06 11:32:32 -0700

    fix: Fix deps and default values

--- utils/parser.js
+++ utils/parser.js
@@ -2,7 +2,7 @@ import { createExportBaseTokens } from "./tokens/createExportBaseTokens.js";
 import { createExportBrandTokens } from "./tokens/createExportBrandTokens.js";
 import { createExportSysTokens } from "./tokens/createExportSysTokens.js";
 
-const { CHANGED = "base,brand,system" } = process.env;
+const { CHANGED } = process.env;
 
 if (CHANGED) {
   if (CHANGED.includes("base")) {
