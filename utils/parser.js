b26508bd2e496a8ab5b1ee02c083674eed7084ea
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2025-10-30 14:49:39 -0600

    chore: Update sync script (#67)

--- utils/parser.js
+++ utils/parser.js
@@ -1,28 +1,3 @@
 import {createExportTokens} from './files/createExportTokens.js';
 
-const {CHANGED} = process.env;
-
-const getTypes = changed => {
-  if (changed === 'all') {
-    return ['base', 'brand', 'sys'];
-  }
-
-  if (changed.includes('base.json')) {
-    return ['base'];
-  }
-
-  if (changed.includes('brand/canvas.json')) {
-    return ['brand'];
-  }
-
-  if (changed.includes('tokens/sys')) {
-    return ['sys'];
-  }
-
-  return [];
-};
-
-if (CHANGED) {
-  const types = getTypes(CHANGED);
-  createExportTokens(types);
-}
+createExportTokens();
