9f92e600f7bacf7b20197e3d9605b74aae4669cb
manuel.carrera <manuel.carrera@workday.com>
2025-11-05 14:17:51 -0700

    fix: Remove extra func

--- utils/files/utils.js
+++ utils/files/utils.js
@@ -262,44 +262,19 @@ export const generatePlatformFiles = (level, allTokens, isDeprecated) => {
 };
 
 /**
- * Recursively read all files in a directory
- * @param {string} dirPath The directory path to read
- * @param {string} basePath The base path for relative file paths
- * @returns {string[]} Array of relative file paths
- */
-const readDirRecursive = (dirPath, basePath = '') => {
-  const files = [];
-  const entries = fs.readdirSync(dirPath, {withFileTypes: true});
-
-  for (const entry of entries) {
-    const fullPath = path.join(dirPath, entry.name);
-    const relativePath = basePath ? `${basePath}/${entry.name}` : entry.name;
-
-    if (entry.isDirectory()) {
-      files.push(...readDirRecursive(fullPath, relativePath));
-    } else if (entry.isFile()) {
-      files.push(relativePath);
-    }
-  }
-
-  return files;
-};
-
-/**
  * Utility function to get the list of tokens files.
  * @returns {string[]} The list of token files
  */
 export const getTokensFilesList = (folderPath, type) => {
-  const tokensDir = path.join(rootDir, 'tokens');
-  const allFilesFromDir = readDirRecursive(tokensDir);
-
-  const allFiles = allFilesFromDir.filter(
-    file =>
-      file.startsWith(folderPath) &&
-      file.endsWith('.json') &&
-      ((type !== 'brand' && !file.includes('brand/')) ||
-        (type === 'brand' && file.includes('brand/')))
-  );
+  const allFiles = fs
+    .readdirSync(path.join(rootDir, 'tokens'), {recursive: true})
+    .filter(
+      file =>
+        file.startsWith(folderPath) &&
+        file.endsWith('.json') &&
+        ((type !== 'brand' && !file.includes('brand/')) ||
+          (type === 'brand' && file.includes('brand/')))
+    );
 
   return allFiles;
 };
