bf66dfe2c5e35206475e3a27fb1ecf572ef8a2ff
manuel.carrera <manuel.carrera@workday.com>
2026-01-14 18:46:58 -0700

    fix: Undo parser changes

--- utils/parser.js
+++ utils/parser.js
@@ -3,68 +3,38 @@ import {
   generateBaseTokens,
   generatePlatformFiles,
   transformStudioTokensToSD,
-  recursivelyCombineTwoObjects,
 } from './files/utils.js';
 
 (() => {
   ['base', 'brand', 'sys'].forEach(type => {
-    const typeFolder = type === 'brand' ? 'sys/brand' : type;
-    
-    // Get non-deprecated tokens
-    const mainTokens = combineTokens(typeFolder, type);
-    
-    // Get deprecated tokens
-    const deprecatedTokens = combineTokens(`deprecated/${typeFolder}`, type);
-    
-    // Combine both token sets - deprecated tokens will be merged into main tokens
-    let combinedTokens = {};
-    
-    // If we have main tokens, start with those
-    if (Object.keys(mainTokens).length) {
-      combinedTokens = mainTokens;
-    }
-    
-    // If we have deprecated tokens, merge them in
-    if (Object.keys(deprecatedTokens).length) {
-      if (Object.keys(combinedTokens).length) {
-        // Merge deprecated tokens into main tokens
-        combinedTokens = recursivelyCombineTwoObjects(combinedTokens, deprecatedTokens);
-      } else {
-        // Only deprecated tokens exist
-        combinedTokens = deprecatedTokens;
+    ['main', 'deprecated'].forEach(category => {
+      const isDeprecated = category === 'deprecated';
+      const typeFolder = type === 'brand' ? 'sys/brand' : type;
+      const importPathFolder = isDeprecated ? `deprecated/${typeFolder}` : typeFolder;
+
+      const tokens = combineTokens(importPathFolder, type);
+
+      // Skip if there are no tokens
+      if (!Object.keys(tokens).length) {
+        return;
       }
-    }
-    
-    // Skip if there are no tokens at all
-    if (!Object.keys(combinedTokens).length) {
-      return;
-    }
-    
-    // Transform tokens
-    transformStudioTokensToSD(combinedTokens);
-    
-    if (type === 'base') {
-      // Base json should be placed to main level
-      generateBaseTokens(combinedTokens, '');
-    } else {
-      // Sys and brand tokens are web specific, so we generate them for platforms
-      generatePlatformFiles(type, combinedTokens, false);
-    }
-  });
-  
-  // Also generate deprecated-only files for backward compatibility if needed
-  ['base', 'brand', 'sys'].forEach(type => {
-    const typeFolder = type === 'brand' ? 'sys/brand' : type;
-    const deprecatedTokens = combineTokens(`deprecated/${typeFolder}`, type);
-    
-    if (Object.keys(deprecatedTokens).length) {
-      transformStudioTokensToSD(deprecatedTokens);
-      
+
+      // It does
+      // - remove figma specific tokens
+      // - flatten the base token object to avoid `base.base.*` references
+      // - change oklch color object to string
+      // - remove studio extensions
+      // - replace TS description with SD comment
+      // - add level token reference to TS
+      transformStudioTokensToSD(tokens);
+
       if (type === 'base') {
-        generateBaseTokens(deprecatedTokens, 'deprecated');
+        // Base json should be placed to main level
+        generateBaseTokens(tokens, isDeprecated ? 'deprecated' : '');
       } else {
-        generatePlatformFiles(type, deprecatedTokens, true);
+        // Sys and brand tokens are web specific, so we generate them for platforms
+        generatePlatformFiles(type, tokens, isDeprecated);
       }
-    }
+    });
   });
 })();
