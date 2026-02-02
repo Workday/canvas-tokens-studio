07cbb5c847210f5919b001ccff45a13cc3033a6b
Raisa Primerova <riyuhide@gmail.com>
2026-02-02 11:10:34 -0700

    fix: Add util scripts

---
+++ utils/ff/get-forward-merge-branch.js
@@ -0,0 +1,28 @@
+import {strict as assert} from 'node:assert';
+
+const {GITHUB_REF = ''} = process.env;
+
+const branch = GITHUB_REF.replace('refs/heads/', '');
+
+const forwardMergeBranches = {
+  main: 'prerelease/minor',
+  'prerelease/minor': 'prerelease/major',
+};
+
+function getForwardMergeBranch(/** @type string */ branch) {
+  const nextBranch = forwardMergeBranches[branch];
+
+  assert(
+    nextBranch,
+    `Could not determine a forward merge branch for "${branch}". Supported branch inputs are ${Object.keys(
+      forwardMergeBranches
+    ).join(', ')}`
+  );
+
+  return nextBranch;
+}
+
+export default getForwardMergeBranch;
+
+// This console log for forward-merge.yml workflow and for workflows that need access to the next branch
+console.log(getForwardMergeBranch(branch));
